23571b836fdbd381088401e9ded8620b2074630e
shyam sunder k <surya46584@gmail.com>
2019-01-03 17:23:44 -0500

    add command line options

--- src/clj/dataviz/core.clj
+++ src/clj/dataviz/core.clj
@@ -3,6 +3,7 @@
             [dataviz.report.core :as report]
             [clojure.tools.cli :refer [parse-opts]]
             [clojure.tools.logging :as log]
+            [cli-matic.core :refer [run-cmd]]
             [mount.core :as mount])
   (:gen-class))
 
@@ -23,16 +24,40 @@
     (log/info component "started"))
   (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
 
-(defn -main [& args]
-  (mount/start #'dataviz.config/env)
-  (log/info "Starting Dataviz app")
-  ;; (start-app args)
-
-  ;; (report/html-to-pdf (select-keys env [:chrome :html-report-file :pdf-report-name]))
-
+(defn load-in-browser
+  "Load the html/js file in the browser"
+  [opt]
   (let [chrome-options (get env :chrome)
         download-dir (select-keys chrome-options [:download-dir])
         driver-opts (select-keys chrome-options [:driver-opts])
         driver-wait-time (select-keys chrome-options [:driver-wait-time])
         html-js-file (select-keys env [:html-js-file])]
-    (report/js-to-pdf (merge download-dir driver-opts html-js-file driver-wait-time))))
+    (report/load-in-browser (merge download-dir driver-opts html-js-file driver-wait-time))))
+
+(defn html->pdf
+  "Convert the input Html to Pdf"
+  [opt]
+  (log/info "html->pdf")
+  (report/html->pdf (select-keys env [:chrome :html-report-file :pdf-report-name])))
+
+(def CONFIGURATION
+  {:app {:command "dataviz"
+         :description "A command-line util to load html in chrome and generate pdf etc"
+         :version "0.0.1"}
+   :commands [{:command "load-in-browser"
+               :description ["Load Html/Js file in the browser"]
+               ;; add dummy options for bug in cli-matic
+               :opts        [{:option "a1" :short "a" :env "AA" :as "First addendum" :type :int :default 0}]
+               :runs load-in-browser}
+              {:command "html-to-pdf"
+               :description ["Convert Html to Pdf"]
+               ;; add dummy options for bug in cli-matic
+               :opts        [{:option "b1" :short "b" :env "A" :as "test" :type :int :default 0}]
+               :runs html->pdf}
+              ]})
+
+(defn -main [& args]
+  (mount/start #'dataviz.config/env)
+  (log/info "Starting Dataviz app")
+  (start-app args)
+  (run-cmd args CONFIGURATION))
--- src/clj/dataviz/report/core.clj
+++ src/clj/dataviz/report/core.clj
@@ -1,6 +1,5 @@
 (ns dataviz.report.core
-  (:require [dataviz.config :refer [env]]
-            [dataviz.report.pdf :as pdf]
+  (:require [dataviz.report.pdf :as pdf]
             [dataviz.browser.etaoin.core :as browser]
             [clojure.tools.logging :as log]))
 
@@ -8,7 +7,7 @@
 ;;   "Generate pdf from the input data in json format"
 ;;   [opts])
 
-(defn html-to-pdf
+(defn html->pdf
   "Generate pdf from the html"
   [opts]
   (let [in (get opts :html-report-file)
@@ -16,8 +15,8 @@
     (log/info "Generating Pdf from Html Report")
     (pdf/html-to-pdf in report-name)))
 
-(defn js-to-pdf
-  "Generate pdf by executing the js code in headless chrome"
+(defn load-in-browser
+  "Load html and js code in headless chrome"
   [opts]
-  (log/info "Generating Pdf by loading Html JS file in Headless chrome")
+  (log/info "Load html and js code in headless chrome")
   (browser/load-html-file opts))
